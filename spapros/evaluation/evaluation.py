c0f0e0e6751eaa1219300d4fb216ac694c0664d0
zethson <lukas.heumos@posteo.net>
2021-07-09 13:54:00 +0200

    possible fix
    Signed-off-by: zethson <lukas.heumos@posteo.net>

--- spapros/evaluation/evaluation.py
+++ spapros/evaluation/evaluation.py
@@ -313,12 +313,15 @@ class ProbesetEvaluator:
 
         # Example file name: gene_corr_small_data_genesets_1_1.csv
         for result_file in result_files:
-            results = pd.read_csv(result_file, index_col=0)
-
             # Assumption for the set ID: last 3 words minus file extension split by _
             set_id = "_".join(result_file[:-4].split("_")[-3:])
             # Assumption for the metric: first 2 words split by _
             metric = "_".join(result_file[:-4].split("_")[:2])
+
+            if (set_id in self.results[metric]) and (self.results[metric][set_id] is not None):
+                results = self.results[metric][set_id]
+            else:
+                results = pd.read_csv(result_file, index_col=0)
 
             summary = metric_summary(
                 adata=self.adata, results=results, metric=metric, parameters=self.metrics_params[metric]
