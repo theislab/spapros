ab251cfcd968a789a483c237a51e48321f83d182
LouisK92 <37270609+LouisK92@users.noreply.github.com>
2022-07-17 20:38:21 +0200

    Add select_DE_genes in __init__

--- spapros/selection/__init__.py
+++ spapros/selection/__init__.py
@@ -1,9 +1,11 @@
 from spapros.selection.selection_procedure import ProbesetSelector
 from spapros.selection.selection_procedure import select_reference_probesets
 from spapros.selection.selection_methods import select_pca_genes
+from spapros.selection.selection_methods import select_DE_genes
 
 __all__ = [
     "ProbesetSelector",
     "select_reference_probesets",
     "select_pca_genes",
+    "select_DE_genes",
 ]
