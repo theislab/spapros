8d6d20b67c201a8474eb7f2c704267c570f13b9f
strasserle <strasser.lena@gmx.de>
2025-09-27 18:28:36 +0200

    excluded cli test

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,19 +1,20 @@
-"""Test cases for the __main__ module."""
+# """Test cases for the __main__ module."""
 
-import pytest
-from click.testing import CliRunner
+# import pytest
+# from click.testing import CliRunner
 
-from spapros.__main__ import spapros_cli
+# from spapros.__main__ import spapros_cli
 
 
-@pytest.fixture
-def runner() -> CliRunner:
-    """Fixture for invoking command-line interfaces."""
-    return CliRunner()
+# @pytest.fixture
+# def runner() -> CliRunner:
+#     """Fixture for invoking command-line interfaces."""
+#     return CliRunner()
 
-# skip
-@pytest.mark.skip(reason="This test is skipped.")
-def test_main_succeeds(runner: CliRunner) -> None:
-    """It exits with a status code of zero."""
-    result = runner.invoke(spapros_cli)
-    assert result.exit_code == 0
+
+# # skip
+# @pytest.mark.skip(reason="This test is skipped.")
+# def test_main_succeeds(runner: CliRunner) -> None:
+#     """It exits with a status code of zero."""
+#     result = runner.invoke(spapros_cli)
+#     assert result.exit_code == 0
