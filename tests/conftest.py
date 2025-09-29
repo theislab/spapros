71cd6c179b1f6b03c75d341a2bc5d460a71aa2ba
strasserle <strasser.lena@gmx.de>
2025-09-29 11:22:15 +0200

    small test fix

--- tests/conftest.py
+++ tests/conftest.py
@@ -77,11 +77,11 @@ def adata_pbmc3k():
     return adata
 
 
-def ref_probeset(adata_pbmc3k, n, geney_key, seeds, verbosity, save_dir, request, reference_selections):
+def ref_probeset(adata_pbmc3k, n, genes_key, seeds, verbosity, save_dir, request, reference_selections):
     se.select_reference_probesets(
         adata_pbmc3k,
         n=n,
-        genes_key="highly_variable",
+        genes_key=genes_key,
         seeds=seeds,
         verbosity=verbosity,
         save_dir=None if not save_dir else request.getfixturevalue(save_dir),
